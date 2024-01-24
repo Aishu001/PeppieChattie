1acba237b44fdb1807a8092f7aa02fdb8bd5b49b
Aishu <sowandaryakrishnan@gmail.com>
2024-01-24 10:10:45 +0530

    changes

--- Server-side/models/user.js
+++ Server-side/models/user.js
@@ -4,6 +4,8 @@ const userSchema = new mongoose.Schema({
     fullName: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
+    authId : {type: String, required: true } ,
+    profileImageUrl: { type: String},
 });
 
 const User = mongoose.model('user', userSchema);
