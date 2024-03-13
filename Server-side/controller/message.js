269b7cc087962580d871d1c3904d4c7e3f4981e5
Aishu <sowandaryakrishnan@gmail.com>
2024-03-13 19:02:38 +0530

    not tested

--- Server-side/controller/message.js
+++ Server-side/controller/message.js
@@ -4,6 +4,18 @@ export const sendMessage = async (req , res) => {
     const { message ,chatId} = req.body;
     if (!message || !chatId) {
         console.log("Invalid data passed into request");
-        return res.sendS(400);
+        return res.send(400);
+    }
+
+    const newMessage = {
+        sender : req.user._id,
+        message: message,
+        chat: chatId
+    }
+    try{
+
+    }
+    catch{
+        
     }
 }
