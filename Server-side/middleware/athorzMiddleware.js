fd43da179371c98b54e48bdeecba18099e335077
Aishu <sowandaryakrishnan@gmail.com>
2024-03-14 00:33:18 +0530

    still middleware

--- Server-side/middleware/athorzMiddleware.js
+++ Server-side/middleware/athorzMiddleware.js
@@ -1,27 +1,32 @@
 import jwt from 'jsonwebtoken';
 
-const authenticateUser = (req, res, next) => {
+const authenticateUser = async(req, res, next) => {
     // Check for Authorization header
     const authHeader = req.headers['authorization'];
     console.log('Authorization Header:', authHeader); // Log the authorization header
     
-    if (!authHeader || !authHeader.startsWith('Bearer ')) {
+    if (!authHeader && !authHeader.startsWith('Bearer ')) {
         console.log('Unauthorized: Token missing or malformed');
         return res.status(401).json({ message: 'Unauthorized' });
     }
 
-    const token = authHeader.substring(7); // Remove the 'Bearer ' prefix
+     const token = authHeader.substring(7); // Remove the 'Bearer ' prefix
     console.log('Extracted Token:', token); // Log the extracted token
-
-    jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+  try {
+   const tokenD =   req.headers.authorization.split(" ")[1];
+   await jwt.verify(tokenD, process.env.JWT_SECRET, (err, user) => {
         if (err) {
             console.error('JWT Verification Error:', err); // Log JWT verification error
             return res.status(403).json({ message: 'Forbidden' });
         }
-        req.user = user;
+        req.user =  user;
         next();
     });
+} catch (e) {
+    res.status(401);
+      throw new Error("Not authorized, token failed");
+}
 };
 
-
 export default authenticateUser;
+
