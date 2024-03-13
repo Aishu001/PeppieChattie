a984f92b889cc4cdd97618d7b58bda7f5efc7820
Aishu <sowandaryakrishnan@gmail.com>
2024-03-14 00:51:48 +0530

    made changes

--- Server-side/middleware/athorzMiddleware.js
+++ Server-side/middleware/athorzMiddleware.js
@@ -1,32 +1,29 @@
 import jwt from 'jsonwebtoken';
+import User from '../models/user.js';
 
-const authenticateUser = async(req, res, next) => {
+const authenticateUser = (req, res, next) => {
     // Check for Authorization header
     const authHeader = req.headers['authorization'];
     console.log('Authorization Header:', authHeader); // Log the authorization header
-    
-    if (!authHeader && !authHeader.startsWith('Bearer ')) {
+
+    if (authHeader && authHeader.startsWith('Bearer ')) {
         console.log('Unauthorized: Token missing or malformed');
         return res.status(401).json({ message: 'Unauthorized' });
     }
 
-     const token = authHeader.substring(7); // Remove the 'Bearer ' prefix
-    console.log('Extracted Token:', token); // Log the extracted token
-  try {
-   const tokenD =   req.headers.authorization.split(" ")[1];
-   await jwt.verify(tokenD, process.env.JWT_SECRET, (err, user) => {
-        if (err) {
-            console.error('JWT Verification Error:', err); // Log JWT verification error
-            return res.status(403).json({ message: 'Forbidden' });
-        }
-        req.user =  user;
+    // const token = authHeader.substring(7); // Remove the 'Bearer ' prefix
+    // console.log('Extracted Token:', token); // Log the extracted token
+
+    try {
+        const token = req.headers.authorization.split(" ")[1]; 
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        req.user =  User.findById(decoded.id).select("-password");
+        ;
         next();
-    });
-} catch (e) {
-    res.status(401);
-      throw new Error("Not authorized, token failed");
-}
+    } catch (error) {
+        console.error('JWT Verification Error:', error); // Log JWT verification error
+        return res.status(403).json({ message: 'Forbidden' });
+    }
 };
 
 export default authenticateUser;
-
