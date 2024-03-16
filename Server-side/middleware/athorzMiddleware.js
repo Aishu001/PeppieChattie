383f17ee049468ce8627f576b020ced7deed6f5c
Aishu <sowandaryakrishnan@gmail.com>
2024-03-16 14:57:39 +0530

    search api worked

--- Server-side/middleware/athorzMiddleware.js
+++ Server-side/middleware/athorzMiddleware.js
@@ -3,7 +3,10 @@ import User from '../models/user.js';
 
 const authenticateUser = async (req, res, next) => {
     // Check for Authorization header
+//   const  JWT_SECRET='MySuperSecretKey123!@'
     const authHeader = req.headers['authorization'];
+   
+
     console.log('Authorization Header:', authHeader); // Log the authorization header
 
     if (authHeader && authHeader.startsWith('Bearer')) {
@@ -11,9 +14,11 @@ const authenticateUser = async (req, res, next) => {
         console.log('Extracted Token:', token); // Log the extracted token
 
         try {
-            const decoded = jwt.verify(token, process.env.JWT_SECRET);
+            console.log('Request Headers:', req.headers);
+            console.log('Request secret key:', process.env.SECRET_KEY);
+            const decoded = jwt.verify(token,process.env.SECRET_KEY);
             console.log('Decoded Token:', decoded); // Log the decoded token payload
-            req.user = await User.findById(decoded.id).select("-password");
+            req.user = await User.findById(decoded.getTheID).select("-password");
             if (!req.user) {
                 console.error('User not found');
                 return res.status(403).json({ message: 'User not found' });
