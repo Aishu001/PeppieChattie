1a647ac0d36b7620d9b2cfbd9ee1c366aee1a143
Aishu <sowandaryakrishnan@gmail.com>
2024-04-03 12:43:56 +0530

    developed home page

--- Client-side/chat-app/src/App.jsx
+++ Client-side/chat-app/src/App.jsx
@@ -3,13 +3,30 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import LandingPage from './Component/LandingPage'
+import { BrowserRouter as Router, Route, Routes  } from "react-router-dom"
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-   <LandingPage/>
+   
+   <Router>
+     <Routes>
+     <Route path="/" element={<LandingPage/>} />
+              <Route path ="/signup" exact Component={SignIn} ></Route>
+              {/* <Route path ="/login" exact Component={Login} ></Route>   
+              <Route path ="/dashboard" exact Component={Dashboard} ></Route>    
+              <Route path ="/contact" exact Component={Contact} ></Route>  
+              <Route path ="/AddContact" exact Component={AddContact} ></Route>    
+              <Route path ="/UpdateContact/:id" exact Component={EditContact} ></Route> 
+              <Route path ="/campaign" exact Component={Campaign} ></Route> 
+              <Route path ="/plan" exact Component={Plan} ></Route> 
+              <Route path="/paymentMode/:title/:price" exact Component={Payment} /> */}
+
+
+            </Routes>
+     </Router>
     </>
   )
 }
--- Client-side/chat-app/src/Component/LandingPage.jsx
+++ Client-side/chat-app/src/Component/LandingPage.jsx
@@ -1,13 +1,60 @@
-import React from 'react'
-import Header from './Header'
+import React from 'react';
+import Header from './Header';
+import { styled } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Grid';
+import { Button, CardActionArea, CardActions } from '@mui/material';
+import '../Style/LandingPage.css'; // Corrected import statement
+
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+  ...theme.typography.body2,
+  padding: theme.spacing(0),
+  textAlign: 'center',
+  color: theme.palette.text.secondary,
+}));
 
 function LandingPage() {
   return (
     <>
-    <Header/>
+      <Header /> {/* Nav Bar*/}
+      <Box sx={{ flexGrow: 1 , backgroundImage: 'linear-gradient(to top, #accbee 0%, #e7f0fd 100%)' }}>
+        <Grid container spacing={2}>
+        
+          <Grid item xs={8} className="center-content">
+          <div className="card-img1">
+          <img src="Peppie.png" alt=""  className="company-img"/>
+          <div  className="text-img1">
+          <Typography variant="body2" color="text.secondary">
+            Lizards are a widespread group of squamate reptiles, with over 6,000
+            species, ranging across all continents except Antarctica
+            <br />
+            <Button size="small" color="primary">
+          Get Start
+        </Button>
+          </Typography>
+          </div>
+          
+              </div>    
+          
     
+         
+          </Grid>
+          <Grid item xs={4} className="center-content">
+         
+          <img src="landingPGG.png" alt="" className="landing-img"/>
+ 
+          </Grid>
+        </Grid>
+      </Box>
     </>
-  )
+  );
 }
 
-export default LandingPage
+export default LandingPage;
