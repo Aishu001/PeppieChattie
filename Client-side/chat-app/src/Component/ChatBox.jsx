8741d264775fc20e45e598774bdbba39c5acf610
Aishu <sowandaryakrishnan@gmail.com>
2024-04-26 02:04:02 +0530

    alignment

--- Client-side/chat-app/src/Component/ChatBox.jsx
+++ Client-side/chat-app/src/Component/ChatBox.jsx
@@ -10,24 +10,15 @@ function ChatBox() {
   return (
    <>
    <div className='box-container'>
+    
     <div className='box-message-container '>
     <h1 className="text-center"></h1>
     </div>  
-    <br />      
-    <div className='box-typing-container '>
-    <Box
-      sx={{
-        '& > :not(style)': { m: 1, width: '1000px' },
-      }}
-     
-    >
-      <TextField label="Enter your message"  />  <span><Fab variant="extended">
-       <SendIcon  />
       
-      </Fab></span>   
-
+    <div className='box-typing-container '>
+    <Box sx={{  '& > :not(style)': { m: 1, width: '1000px' }, }} >
+      <TextField label="Enter your message"  />  <span><Fab variant="extended"><SendIcon/></Fab></span>   
     </Box>
-
     </div>
    </div>
   
